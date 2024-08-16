396a4b573b02150a6097d07359b5085b4ad04347
Valentin Marquez <valentin13.mail@gmail.com>
2024-08-15 20:18:19 -0400

    Exercise 1.11*: unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 
-const StatisticLine = ({ text, value }) => {
+const StatisticLine = (props) => {
+  const { text, value } = props
   return (
-    <p>{text}: {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -10,15 +14,16 @@ const StatisticLine = ({ text, value }) => {
 const Statistics = (props) => {
   const { good, neutral, bad, all, average, positive } = props
   return (
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive + ' %'} />
-
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + ' %'} />
+      </tbody>
+    </table>
   )
 }
 
